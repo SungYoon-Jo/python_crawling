01a8abac624f3db48353949b6aa404193774d2c6
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-05-19 00:25:30 +0900

    add a.py

--- normal/p.py
+++ normal/p.py
@@ -23,17 +23,17 @@ r = soup.select('.tF2Cxc')  #원하는 class / name을 F12에서 찾기 # select
 
 print(type(r))
 
-for page in range(3,7):
-    time.sleep(3)
+# for page in range(3,7):
+#     time.sleep(3)
     
-    # for i in r :  
-    #     print(i.select_one('.LC20lb.MBeuO.DKV0Md').text) #제목 #select one을 사용하면 텍스트를 가져올 수 있다. #클래스에 빈칸은 점으로 바꿔준다.
-    #     print(i.a.attrs['href']) #링크 #a 태그 안에, href 를 속성을 갖는 링크 불루직
-    #     print()
+for i in r :  
+    print(i.select_one('.LC20lb.MBeuO.DKV0Md').text) #제목 #select one을 사용하면 텍스트를 가져올 수 있다. #클래스에 빈칸은 점으로 바꿔준다.
+    print(i.a.attrs['href']) #링크 #a 태그 안에, href 를 속성을 갖는 링크 불루직
+    print()
    
     ## 다음 페이지 클릭
     # driver.find_element_by_xpath('//*[@id="pageingWrap"]/div/div[1]/a[%s]' %page).click()
-    driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
+    # driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
 
     # //*[@id="xjs"]/table/tbody/tr/td[2]
 
