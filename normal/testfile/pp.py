6c695a5c749a5b5d00de056a13b254ebdd9d1e42
JoSeongYoon <whtjddbs0112@naver.com>
2022-05-24 22:27:42 +0900

    good

--- normal/testfile/pp.py
+++ normal/testfile/pp.py
@@ -11,9 +11,9 @@ baseurl = 'https://www.google.com/search?q='
 pluseurl = input('검색어를 터미널에서 입력하세요 : ')
 url = baseurl + urllib.parse.quote_plus(pluseurl)
 
-filename = 'test1.csv'
-f = open(filename, 'w', encoding='utf-8-sig', newline='')
-writer = csv.writer(f)
+# filename = 'test1.csv'
+# f = open(filename, 'w', encoding='utf-8-sig', newline='')
+# writer = csv.writer(f)
 
 print(url) # https://www.google.com/search?q=입력데이터(pluseurl)
 
@@ -30,21 +30,25 @@ r = soup.select('.tF2Cxc')  #원하는 class / name을 F12에서 찾기 # select
 for page in range(3,4):
     time.sleep(3)
     for i in r :
-  
+        # print(i)
+        # data = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
+
+        driver.find_element(i.a['href']).click()
+        
+        # print()
+        # writer.writerow(data)
+
         # print(i.select_one('.LC20lb.MBeuO.DKV0Md').text) #제목 #select one을 사용하면 텍스트를 가져올 수 있다. #클래스에 빈칸은 점으로 바꿔준다.
-        # writer.writerow(i.select_one('.LC20lb.MBeuO.DKV0Md').text)
-        data = (i.select_one('.LC20lb.MBeuO.DKV0Md').text)
         # print(i.a.attrs['href']) #링크 #a 태그 안에, href 를 속성을 갖는 링크 불루직
         # print()
-        writer.writerow(data)
-    ## 다음 페이지 클릭
-    # driver.find_element_by_xpath('//*[@id="pageingWrap"]/div/div[1]/a[%s]' %page).click()
+        # writer.writerow(i)
+
+    # 다음 페이지 클릭
     # driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
 
-    # //*[@id="xjs"]/table/tbody/tr/td[2]
 
-# if (driver.close() == 1):
-#     print('good2')
 #크롬 드라이버 닫아주기
-
 driver.close()
+
+# 실행 확인
+print("good sccess")
