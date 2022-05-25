ef79bd23f9170e4b896e3f7f3c6858fa8062c4af
JoSeongYoon <whtjddbs0112@naver.com>
2022-05-25 12:56:54 +0900

    ...

--- normal/testfile/pp.py
+++ normal/testfile/pp.py
@@ -31,26 +31,29 @@ r = soup.select('.tF2Cxc')  #원하는 class / name을 F12에서 찾기 # select
 for page in range(3,4):
     time.sleep(3)
     for i in r :
-        # data = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
-        # columns = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
-        # columns = i.find_all('.LC20lb.MBeuO.DKV0Md')
-        # columns = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
-        # columns = i.select.one('.LC20lb.MBeuO.DKV0Md')
-        # dt = [column.text() for column in columns[:]]
-        print(r)
+    # #     # data = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
+    # #     # columns = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
+    # #     # columns = i.find_all('.LC20lb.MBeuO.DKV0Md')
+    # #     # columns = (i.select_one('.LC20lb.MBeuO.DKV0Md').text,i.a.attrs['href'])
+    # #     # columns = i.select.one('.LC20lb.MBeuO.DKV0Md')
+    # #     # dt = [column.text() for column in columns[:]]
+    #     print(r)
         # writer.writerow(data)
 
         # print(i.select_one('.LC20lb.MBeuO.DKV0Md').text) #제목 #select one을 사용하면 텍스트를 가져올 수 있다. #클래스에 빈칸은 점으로 바꿔준다.
+        # print(i.select('.LC20lb.MBeuO.DKV0Md'))
+        # print(i.a.attrs['href'])
+        # print(i.a.attrs)
         # print(i.a.attrs['href']) #링크 #a 태그 안에, href 를 속성을 갖는 링크 불루직
-        # print()
-        # writer.writerow(i)
+        print()
+        writer.writerow(i.a.attrs['href'])
 
     # 다음 페이지 클릭
     # driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
 
 
 #크롬 드라이버 닫아주기
-# driver.close()
+driver.close()
 
 # 실행 확인
 print("good sccess")
