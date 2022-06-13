b4607d315a3f587e29a8fc99d2a49879fefefa80
JoSeongYoon <whtjddbs0112@naver.com>
2022-06-13 16:09:35 +0900

    test google에 간단한 내용 추가

--- normal/testfile/google.py
+++ normal/testfile/google.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import urllib.parse
+import webbrowser
 
 # selenium 안에 webdriver를 사용하기 위해서 웹 사이트를 띄운다
-driver = webdriver.Chrome()
+
 
 # csv파일로 저장 할수 있도록 한다. 이름, 파일 데이터 형태, 저장 순으로 초기화 해준다.
 filename = 'google.csv'
@@ -13,9 +14,9 @@ f = open(filename, 'w', encoding='utf-8-sig', newline='')
 writer = csv.writer(f)
 
 con = 0
-num = 30
+num = 10
 
-# con = 0 부터 num 30까지 10씩 증가하며 반복
+# con = 0 부터 num 30까지 10씩 증가하며 반복 0 = 1p 10 = 2p 20 = 3p ...
 for pg in range(con,num,10):
     time.sleep(4)
     print(pg)
@@ -23,10 +24,13 @@ for pg in range(con,num,10):
     if (pg == 0):
         pluseurl = input('검색어를 터미널에서 입력하세요 : ')
         url = 'https://www.google.com/search?q=%s' %(pluseurl)
+        driver = webdriver.Chrome()    
 
     ch = "&start=%d" % (pg)
     churl = url + ch
 
+    # driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
+ 
     driver.get(churl)
     html = driver.page_source
     soup = BeautifulSoup(html,'html.parser')
@@ -36,6 +40,12 @@ for pg in range(con,num,10):
         temp.append(i.select_one('.LC20lb.MBeuO.DKV0Md').text)
         temp.append(i.a.attrs['href'])
         writer.writerow(temp)
+        # print(temp[1])
+        webbrowser.open(temp[1]) # 0번째 웹 url 열기, 문제: 한개만 여는것이 아니라 한번에temp[1]의 내용을 열어버림, 해결책: 하나씩 열어서 하나씩 닫아야함 또한 url을 하나씩 할당 해줘야함
+
+        
+    
+
 
 #크롬 드라이버 닫아주기
 driver.close()
@@ -43,10 +53,8 @@ driver.close()
 # 그냥 실행 확인
 print("\ngood sccess")
 
-
-# 추가 기능 동적 url, next page, click url and inside url 캡처
-
-
+# 추가 해야 할 기능 
+# click url and inside url 캡처
 
 # 참고 자료
 
