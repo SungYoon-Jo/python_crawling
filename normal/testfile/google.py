f6ba71aa3666cfc17e7328b84d1d424de17224fe
JoSeongYoon <whtjddbs0112@naver.com>
2022-06-22 13:45:48 +0900

    v1.0

--- normal/testfile/google.py
+++ normal/testfile/google.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 import time
 import urllib.parse
 import webbrowser
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # selenium 안에 webdriver를 사용하기 위해서 웹 사이트를 띄운다
 
@@ -18,9 +21,8 @@ num = 10
 
 # con = 0 부터 num 30까지 10씩 증가하며 반복 0 = 1p 10 = 2p 20 = 3p ...
 for pg in range(con,num,10):
-    time.sleep(4)
     print(pg)
-    # url = "https://www.google.com/search?q=apple&ei=CKmVYveQMpqJoAS7xbuQAg&start=%d&sa=N&ved=2ahUKEwj3tIuUgon4AhWaBIgKHbviDiIQ8tMDegQIAxA9&biw=1036&bih=666&dpr=1.25"% pg
+    # url = "https://www.google.com/search?q=apple&ei=CKmVYveQMpqJoAS7xbuQAg&start=%d&sa=N&ved=2ahUKEwj3tIuUgon4AhWaBIgKHbviDiIQ8tMDegQIAxA9&biw=1036&bih=666&dpr=1.25"% pg #소스페이지
     if (pg == 0):
         pluseurl = input('검색어를 터미널에서 입력하세요 : ')
         url = 'https://www.google.com/search?q=%s' %(pluseurl)
@@ -28,24 +30,27 @@ for pg in range(con,num,10):
 
     ch = "&start=%d" % (pg)
     churl = url + ch
-
-    # driver.find_element_by_xpath('//*[@id="xjs"]/table/tbody/tr/td[%s]' %page).click()
- 
     driver.get(churl)
+    # time.sleep(4)
+    wait = WebDriverWait(driver,5)
+
     html = driver.page_source
     soup = BeautifulSoup(html,'html.parser')
     r = soup.select('.tF2Cxc') 
+
+
     for i in r:
         temp = []
-        temp.append(i.select_one('.LC20lb.MBeuO.DKV0Md').text)
-        temp.append(i.a.attrs['href'])
-        writer.writerow(temp)
-        # print(temp[1])
-        webbrowser.open(temp[1]) # 0번째 웹 url 열기, 문제: 한개만 여는것이 아니라 한번에temp[1]의 내용을 열어버림, 해결책: 하나씩 열어서 하나씩 닫아야함 또한 url을 하나씩 할당 해줘야함
-
-        
-    
-
+        # print(i)
+        # temp[i].append(i.select_one('.LC20lb.MBeuO.DKV0Md').text)
+        # temp.append(i.a.attrs['href'])
+        temp = i.a.attrs['href']
+        # print(len(temp))
+        print(temp[:])
+        # writer.writerow(temp[])
+        # webbrowser.open(temp) 
+        # 0번째 웹 url 열기, 문제: 한개만 여는것이 아니라 한번에temp[1]의 내용을 열어버림, 해결책: 하나씩 열어서 하나씩 닫아야함 또한 url을 하나씩 할당 해줘야함
+        # 한개를 누르고 사진을 받아오고 닫고 다음거 누르고 받고 닫고
 
 #크롬 드라이버 닫아주기
 driver.close()
---
+++ normal/testfile/testpy.py
@@ -0,0 +1,4 @@
+for i in range(2):
+    temp = []
+    temp = 10,20
+print(temp[0])    
