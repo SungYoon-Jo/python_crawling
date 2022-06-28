59e5847ec07cffe0d4f57e560980df11022230c9
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-06-28 14:49:37 +0900

    ...

--- module/batch.py
+++ module/batch.py
@@ -1,6 +1,13 @@
 import os
 
-os.system('start.bat')
+os.popen("start.bat")
+
+# if op == 0:
+#     os.system('start.bat')
+#     print("good")
+# else:
+#     print("not")
+#     os.system('start.bat')
 
 # if __name__ == '__main__':   
 #     os.system('start.bat')
