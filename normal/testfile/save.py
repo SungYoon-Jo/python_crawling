71c82189aa0e8d0fb5fc8cda8c17ed34874cbde2
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-01 13:50:20 +0900

    문자열 나누기가 안됨

--- normal/testfile/save.py
+++ normal/testfile/save.py
@@ -1,3 +1,4 @@
+from msilib.schema import Class
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,13 +7,13 @@ import csv
 from selenium.webdriver.common.by import By
 
 
-filename = 'html.csv'
+filename = 'hs.csv'
 f = open(filename, 'w', encoding='utf-8-sig', newline='')
-writer = csv.writer(f)
+hs = csv.writer(f)
 
 fn = 'page.csv'
 fi = open(fn, 'w', encoding='utf-8-sig', newline='')
-wr = csv.writer(fi)
+page = csv.writer(fi)
 
 chrome_options = Options()  
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
@@ -42,35 +43,75 @@ for pg in range(con,num,10):
     # r = soup.select('.tF2Cxc')
     # time.sleep(3)
     # rr = soup.select_one('.tjvcx')
-    rr = soup.select('.NJjxre')
+    rr = soup.select('.jtfYYd')
     # r = driver.find_element(By.CLASS_NAME, "srp")
     # print(r)
     # srp, tF2Cxc, NJjxre, jtfYYd, tjvcx
     # writer.writerow(r) 
-    wr.writerow(rr)
+    # pn = rr[::2]
+    for ii in rr:
+        tag = []
+        tag.append(ii.select_one('.NJjxre').text)
+        pn = tag[:]
+        # tag = ii.select_one('.NJjxre').text
+        # for i in len(tag):
+        hs.writerow([pn])
 
-    for i in rr:
-        temp = []
+        # pn = tag[::]
+        # print(pn)
+
+
+    # tag.get_text()
+    
+    # pn = rr[::2]
+
+
+
+    # wr.writerow(rr)
+
+    # for i in rr:
+    #     for tag in pn:
+
+
+
+
+    #     temp = []
+
         # if i.select_one('.qzEoUe'):
         #     continue
         # else:
         #     temp.append(i.select_one('.tjvcx').text.find(http)) # text[0] = 0번째만 출력
-        # temp.append(i.select_one('.tjvcx').text)
-        a = i.select('.tjvcx')[0].text
+        # a = temp.append(i.select_one('.tjvcx').text)
+        
+
+        # print(a)
+        # print(string.split(maxsplit=2))
+
+        # print(temp.split(' '))
+        
+
+
+        # temp.append(i.select_one('.LC20lb MBeuO DKV0Md'))
+        # temp.extend(i.select_one('.tjvcx').text)
+        # a = i.select('.tjvcx')[0].text
+        # temp.append(i.select('.tjvcx').text)
         # print(a)
         # temp = i.select_one('.tjvcx').text
         # print(cu)
 
+        # val = "https" in temp
         
-
-
+            # print(temp[0])
+        # print(val)
+        
+        # if 
 
         # temp.append(i.select('.tjvcx'))
         # temp.append(i.a.attrs['href'])
         # temp.append(aaa)
-        writer.writerow(a)
+
+        # html.writerow(temp)
 
  
-    # print(cu)
 
 # f.close()
--- normal/testfile/testpy.py
+++ normal/testfile/testpy.py
@@ -1,6 +1,19 @@
-myHost = "webisfree.com"
-search = ".com"
-if search in myHost:
-  print("good")
-else:
-  print("not")
+string = 'I became a zombie'
+ 
+# 한 문자씩 나누기 -> 리스트
+list(string) # list() 함수 이용
+
+ 
+ # 'a'를 구분자로 쪼갭니다.
+ 
+print(string.split(' '))
+
+# maxsplit은 최대 몇번 쪼갤지
+# 처음 으로 나오는 공백 I 와 became사이를 쪼갭니다.
+
+print(string.split(maxsplit=2))
+ 
+# ## 다시 합칠 수도 있음
+# (',').join(string.split()) # ','를 구분자로 합치기
+ 
+# (' ').join(string.split()) # 공백을 구분자로 합치기 -> 원래 문자열
