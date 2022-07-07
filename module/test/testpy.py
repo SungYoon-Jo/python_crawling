12f8b3e99f3d9ba89b5ee17228a50e6de1c65b37
JoSungYun <37609646+joseongyoon@users.noreply.github.com>
2022-07-07 14:05:55 +0900

    기능은 다 만들었고 최적화만 하면 됨 일단 기능 사용해서 실적 올리기

---
+++ module/test/testpy.py
@@ -0,0 +1,16 @@
+a = list
+b = []
+
+
+# a.append(rkawk)
+
+# b.append("rkawk")
+# print(b)
+
+
+for i in range(0,3):
+    c = input("입력 하시오 : ")
+    b.append(c)
+    # b = c
+
+print(b)
--- normal/testfile/indata.py
+++ normal/testfile/indata.py
@@ -4,6 +4,8 @@ from format import *
 import csvmodule as cm
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
+# https://www.nuricops.org/member/login/loginView.do -> 2번째 로그인 페이지
+# https://www.nuricops.org/declaration/webreg/webregWriteView.do -> 신고 페이지
 
 # 로그인확인도 해야함
 
@@ -13,45 +15,50 @@ def urldata():
     
     if url == curl:
         print("good")
+        # driver.get(url)
     elif url != curl:
         url = 'https://www.nuricops.org/declaration/webreg/webregWriteView.do'
         driver.get(url)
         driver.implicitly_wait(5)
 
+
     file = "data.csv"
     df = pd.read_csv(file, sep='\t', names=["href: "])
     
     at = []
-    gab = []
-    
+    # tm = []
+
     for i in df.index:
         print(i)
         driver.find_element(By.XPATH, """//*[@id="radio1"]""").click()
 
         inurl = driver.find_element(By.ID, "acuseUrl")
         inurl.clear()
+        # print(df.loc[i])
 
         inurl.send_keys(df.loc[i])
         driver.find_element(By.XPATH, """//*[@id="btnUrlCheck"]""").click()
         time.sleep(1)
 
         at.append(Alert(driver).text)
-        Alert(driver).accept() # ALERT 확인 누르기
-
-        if 'URL' in at[i]:
-            gab.append("GOOD")
-        else:
-            gab.append("BAD")
-
-
+        # tm = Alert(driver).text # 텍스쳐 받아오기
+        # at = driver.switch_to.alert
+        # tm = at.text
+        # at.accept()
+        Alert(driver).accept() # 누르기
+        
         inurl.clear()
         time.sleep(1)
 
-    dataf = pd.DataFrame(cm.writer, columns=['URL'])
-    dataf['GOB TEXT'] = at
-    dataf['GOOD AND BAD'] = gab
+        # print(at[i])
+        # print()
+
+    dataf = pd.DataFrame(cm.writer, columns=['URL 주소'])
+    dataf['가능여부'] = at
     dataf.to_csv("tp.csv", index=True, encoding="utf-8-sig")
 
+    # cm.gd.writerow(cm.writer)
+    # cm.gd.writerow(at)
     cm.gof.close()
 
 
