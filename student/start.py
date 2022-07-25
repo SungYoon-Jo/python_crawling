7e8b7ea9779860f090efad26a3775be6248f1bc9
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2022-07-25 12:04:29 +0900

    1212

--- student/start.py
+++ student/start.py
@@ -2,12 +2,14 @@ import time
 from format import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
-
-# 로그인확인도 해야함
-
+import keyboard
 
 def urldata():
     curl = driver.current_url
+    hand = driver.window_handles
+    
+    leng = len(hand)+1
+
     url = 'http://sugang.tu.ac.kr/login/login.aspx'
 
     if url == curl:
@@ -17,43 +19,50 @@ def urldata():
         driver.get(url)
         driver.implicitly_wait(5)
     
-    i = 0
-    while i < 10: 
-        print("check number : ", i)
+    print(leng)
 
-        try:
-            print("try")
-            driver.find_element(By.NAME, "user_id")
-
-            login = driver.find_element(By.NAME, "user_id")
-            # myid = input('아이디를 입력해주세요 : ')
-            myid = "18110140"
-            login.send_keys(myid)
-
-            # mypw = input('패스워드를 입력해주세요 : ')
-            mypw = "18whtjd!@"
-            login = driver.find_element(By.NAME, "password")
-            login.send_keys(mypw)
-
-            driver.find_element(By.XPATH, """//*[@id="WebForm1"]/table/tbody/tr[2]/td[1]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]/img""").click()
-            
-            time.sleep(1)
-
-            driver.get(curl)
-            print(driver.current_url)
-            # Alert(driver).accept()
-
-        except:
-            print("except")
-            driver.get(curl)
-            print(driver.current_url)
-            # Alert(driver).accept()
-
-        i += 1
-
-        if (i == 3):
+    while True: 
+        print("start~")
+        if keyboard.is_pressed("q"):
+            print("You pressed q")
             break
 
-        # time.sleep(1)
+        for i in range(0,leng):
+
+            if url != curl:
+                url = 'http://sugang.tu.ac.kr/login/login.aspx'
+                driver.get(url)
+                driver.implicitly_wait(5)
+
+            print(i)
+
+            if i == 3:
+                i = 0
+                continue
+
+            try:
+                driver.find_element(By.NAME, "user_id")
+
+                login = driver.find_element(By.NAME, "user_id")
+                # myid = input('아이디를 입력해주세요 : ')
+                myid = "18110140"
+                login.send_keys(myid)
+
+                # mypw = input('패스워드를 입력해주세요 : ')
+                mypw = "18whtjd!@"
+                login = driver.find_element(By.NAME, "password")
+                login.send_keys(mypw)
+
+                driver.find_element(By.XPATH, """//*[@id="WebForm1"]/table/tbody/tr[2]/td[1]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]/img""").click()
+                
+                time.sleep(0.3)
+
+                last_tab = hand[i]
+                # time.sleep(0.5)
+                driver.switch_to.window(last_tab)
+            except:
+                url = 'http://sugang.tu.ac.kr/login/login.aspx'
+                driver.get(url)
+                driver.implicitly_wait(5)
 
     print("end good day")
