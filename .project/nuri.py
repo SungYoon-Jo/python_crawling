fd42bd5999c13ddd01c95188617aace8d9feaedb
Jo  Seongyun <whtjddbs0112@naver.com>
2022-10-30 23:59:03 +0900

    ...

--- .project/nuri.py
+++ .project/nuri.py
@@ -13,6 +13,8 @@ import csv
 chrome_options = Options()  
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
 
 # url = 'https://www.nuricops.org/declaration/webreg/webregWriteView.do'
 # driver.get(url)
