0ecab0bb7e4da649f37b0ddd63251b259425ffaa
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2022-10-31 16:10:00 +0900

    사진 첨부까지 가능

--- .project/nuri.py
+++ .project/nuri.py
@@ -4,10 +4,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.alert import Alert
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 # import csvmodule as cm
 import csv 
+from PIL import Image
+import cv2
+
 
 
 chrome_options = Options()  
@@ -22,6 +26,11 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 # file = input("실행하게 될 CSV 파일을 입력해주세요 : ")
 file = "file"
 df = pd.read_csv('./csvdb/' + file + '.csv', sep='\t', names=["href"])
+# im = pd.read_csv('./img/' + file + '.png', sep=' \t', names=["href"])
+# im = pd.read('./img/' + 'img.png')
+# img = Image.open("./img/img.png")
+# img = cv2.imread("./img/img.png")
+# print(img)
 
 filename = "file"
 ff = open('./csvdb/' + filename + '.csv', 'r', encoding='utf-8-sig', newline='')
@@ -31,7 +40,7 @@ writer = csv.reader(ff)
 
 def clickRadio():
 
-    driver.find_element(By.XPATH, """//*[@id="radio1"]""").click()
+    driver.find_element(By.XPATH, """//*[@id="radio2"]""").click()
 
     print("radio")
 
@@ -43,6 +52,8 @@ def inputUrl():
     
     for i in df.index:
         
+        driver.find_element(By.XPATH, """//*[@id="radio4"]""").click()
+        
         print(i)
         
         inurl = driver.find_element(By.ID, "acuseUrl")
@@ -52,11 +63,17 @@ def inputUrl():
         driver.find_element(By.XPATH, """//*[@id="btnUrlCheck"]""").click()
         time.sleep(1)
 
-
+        
         alert.append(Alert(driver).text)
         Alert(driver).accept()
         
         inurl.clear()
+        
+        # driver.find_element(By.CSS_SELECTOR,"input[type='file']").send_keys(img)
+        driver.find_element(By.CSS_SELECTOR,"input[type='file']").send_keys(r"C:\Users\whtjd\Documents\GitHub\python\.project\img\img.png")
+        # driver.find_element_by_css_selector("input[type='file']").send_keys(r"C:\Users\user-pc\Desktop\img.PNG")
+        # driver.find_element_by_css_selector("input[type='file']").send_keys(r"C:\Users\whtjd\Documents\GitHub\python\.project\img\img.png")
+        
         time.sleep(1)
 
 
